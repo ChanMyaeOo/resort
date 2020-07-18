4ae42499aa0364ef277daa946d08cc7c0c812951
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-07-18 10:43:32 +0630

    first commit of react resort

---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Hero from "../components/Hero";
+const Home = () => {
+  return <Hero></Hero>;
+};
+export default Home;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class SingleRoom extends React.Component {
+  render() {
+    return <div>Hello from single room page</div>;
+  }
+}
+export default SingleRoom;
